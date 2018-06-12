cf445c03d83aacc6a68431cfc78a09dfcba66740
Joris Langlois <joris.langlois@knplabs.com>
2018-06-12 11:16:26 +0200

    Created the view course handler.

---
+++ src/Command/Handler/ViewCourseHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Command\Handler;
+
+use App\Command\Handler\ICommandHandler;
+use App\Command\ICommand;
+
+class ViewCourseHandler implements ICommandHandler
+{
+    public function handle(ICommand $command)
+    {        
+    }
+}
